b97d4667c004353e4f0836103ad38b21979e0d4c
xosemanolo <xose.manolo.rodriguez@gmail.com>
2020-03-29 20:22:42 -0400

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import '../assets/css/App.css'
 import React, { Component } from 'react'
+import Test from './test'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello, Electron!</h1>
-
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+        <Test />
       </div>
     )
   }
---
+++ src/components/test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <p id="one">inner test</p>
+      </div>
+    )
+  }
+}
+
+export default Test
